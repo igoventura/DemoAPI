3228d2d50d4b5bb3c42233c7c9314dd51702a83c
Igo Ventura <igoventura@outlook.com>
2021-08-07 12:03:14 -0300

    defining base projects

---
+++ DemoAPI.Domain/Entities/Transactions.cs
@@ -0,0 +1,10 @@
+﻿namespace DemoAPI.Domain.Entities
+{
+    public class Transactions
+    {
+        public int Id { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+    }
+}
